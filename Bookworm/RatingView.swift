7d890856de0ba77a61817f7f65bd0edf115f9b04
Masoud Safari <mmsafari@gmail.com>
2024-03-07 10:27:06 -0800

    Add RatingView to show rating as stars.

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,51 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Masoud Safari on 2024-03-07.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                Button {
+                    rating = number
+                } label: {
+                    image(for: number)
+                        .foregroundStyle(number > rating ? offColor : onColor)
+                }
+            }
+        }
+        .buttonStyle(.plain) // to disable the tap on the whole box
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            offImage ?? onImage
+        } else {
+            onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(4))
+}
